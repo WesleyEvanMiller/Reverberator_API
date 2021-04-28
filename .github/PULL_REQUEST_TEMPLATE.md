1788c4931d43546af0fa46b9959cadfe25c3e0fb
Wesley <wesmiller94@live.com>
2021-04-28 12:38:34 -0400

    Add project files

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,9 @@
+{pullDescription}
+
+#Before Marking PR as Ready
+
+- [ ] Update Changelog.md
+- [ ] Update README.md
+- [ ] Bump iOS and Android versions
+
+Closes {issueNumer}
---
+++ Reverberator_API/Controllers/ReverberatorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reverberator_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReverberatorController : ControllerBase
+    {
+        private readonly ILogger<ReverberatorController> _logger;
+
+        public ReverberatorController(ILogger<ReverberatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
